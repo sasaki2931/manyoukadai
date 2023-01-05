0c64dd48e3bcd64ffc1cf44d9bbe05c1bcce732f
syunsuke sasaki <sasaki2931@gmail.com>
2023-01-05 18:09:35 +0900

    createメソッド追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,29 @@
 class TasksController < ApplicationController
     def index
+      @tasks = Task.all
     end
 
     def new
-        @task = Task.new
+      @task = Task.new
     end
+
+    def create
+      Task.create(task_params)
+      redirect_to new_task_path
+    end
+
+
 
     def show
     end
 
     def edit
+    end
+
+    private
+    
+    def task_params
+        params.require(:task).permit(:name,:content)
     end
 
 end
