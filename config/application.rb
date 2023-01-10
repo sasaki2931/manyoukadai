c8b01ab4d0c23e937bfebd17aa77b301e49a4b9c
syunsuke sasaki <sasaki2931@gmail.com>
2023-01-10 13:17:30 +0900

    タイムゾーン設定

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,8 @@ Bundler.require(*Rails.groups)
 
 module Monyou
   class Application < Rails::Application
-    
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.load_defaults 6.1
     config.generators do |g|
       g.test_framework :rspec,
